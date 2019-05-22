d0713da982b55e37410e46c93604d5957295e4e7
mtBrieger <48014174+mtBrieger@users.noreply.github.com>
2019-05-22 12:02:18 +0100

    initial commit

---
+++ dissect_custom_sec.py
@@ -0,0 +1,68 @@
+
+import sys
+from Crypto.Cipher import AES
+
+# -> 0E 03 A0 11 68EA14 1ED017 02 01C80000 (CMD)
+# <- 11 03 A0 02 1ED017 68EA14 04 D962D9FB2B03 00 (CAL)
+# -> 19 03 A0 03 68EA14 1ED017 344305154D33A8766DBAE938311FA514 (RSP)
+# <- 12 03 80 02 1ED017 68EA14 0101C80016 8B0C277F (ACK)
+
+# AES-key: A4E375C6B09FD185F27C4E96FC273AE4
+
+
+key = bytes.fromhex('A4E375C6B09FD185F27C4E96FC273AE4')
+
+
+def xor(a, b):
+    c = [0] * (len(a))
+    for i in range(len(a)):
+        if len(b) > i:
+            c[i] = (a[i] ^ b[i]) & 0xff
+        else:
+            c[i] = a[i]
+    return bytes(c)
+
+
+def format_bytes(a):
+    return ''.join(format(x, '02x') for x in a)
+
+
+def bits_to_bytes(s):
+    return int(s, 2).to_bytes(len(s) // 8, byteorder='big')
+
+
+def bitstring_to_bytes(s):
+    v = int(s, 2)
+    b = bytearray()
+    while v:
+        b.append(v & 0xff)
+        v >>= 8
+    return bytes(b[::-1])
+
+
+def main():
+    f = sys.stdin
+    f.readline()
+    f.readline()
+
+    m_frame = bitstring_to_bytes(f.readline()[-97:-1])
+    c_frame = bitstring_to_bytes(f.readline()[-145:-1])
+
+    #print('m_frame: ' + format_bytes(m_frame))
+    #print('c_frame: ' + format_bytes(c_frame))
+
+    tmp = xor(key, c_frame[11:17])
+    aes = AES.new(tmp, AES.MODE_ECB)
+
+    Pd_d = bytes.fromhex('00' * 6) + m_frame[1:11]
+    Pd_ = aes.encrypt(Pd_d)
+
+    parameters = m_frame[11:15]
+    Pd = xor(Pd_, parameters)
+    P = aes.encrypt(Pd)
+
+    print(''.join(format(x, '08b') for x in P), end='')
+
+
+if __name__ == "__main__":
+    main()
